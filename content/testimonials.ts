1d348aa874f17080a631ec228d06b422fb2f2916
Tyler Williams <tyler@coolsoftware.dev>
2021-04-09 20:35:54 -0600

    add second set of data, demo it in a second slider, add missing keys

--- content/testimonials.ts
+++ content/testimonials.ts
@@ -1,21 +1,40 @@
-const Testimonials = [
-    {
-        background: "/images/CarTalk-Testimonial-Image.jpg",
-        backgroundOverlay: "bg-alkali-600",
-        company: "CarTalkRepair",
-        logo: "/images/CarTalk-Repair.png",
-        name: "Cody Lintz, Owner",
-        quote: "Starting off as a new business we realized the importance of having a sound online presence early on. We are extremely grateful we found Alkali we did. They have provided us the tools we need to adequately serve our customers."
-    },
-    {
-        background: "/images/carro-testimonial-bg.jpg",
-        backgroundOverlay: "bg-pink-300",
-        company: "Carro",
-        logo: "/images/carro-logo-white.png",
-        name: "Eric Sanchez, Operations Manager",
-        quote: "Client testimonial Client testimonial Client testimonial Client testimonial Client testimonial Client testimonial.Client testimonial Client testimonial Client testimonial Client testimonial Client testimonial Client testimonial."
-    },
-
-]
+const Testimonials = {
+    development: [
+        {
+            background: "/images/CarTalk-Testimonial-Image.jpg",
+            backgroundOverlay: "bg-alkali-600",
+            company: "CarTalkRepair",
+            logo: "/images/CarTalk-Repair.png",
+            name: "Cody Lintz, Owner",
+            quote: "Starting off as a new business we realized the importance of having a sound online presence early on. We are extremely grateful we found Alkali we did. They have provided us the tools we need to adequately serve our customers."
+        },
+        {
+            background: "/images/carro-testimonial-bg.jpg",
+            backgroundOverlay: "bg-pink-300",
+            company: "Carro",
+            logo: "/images/carro-logo-white.png",
+            name: "Eric Sanchez, Operations Manager",
+            quote: "Client testimonial Client testimonial Client testimonial Client testimonial Client testimonial Client testimonial.Client testimonial Client testimonial Client testimonial Client testimonial Client testimonial Client testimonial."
+        },
+    ],
+    design: [
+        {
+            background: "/images/CarTalk-Testimonial-Image.jpg",
+            backgroundOverlay: "bg-alkali-600",
+            company: "CarTalkRepair - Design",
+            logo: "/images/CarTalk-Repair.png",
+            name: "Cody Lintz, Owner",
+            quote: 'Here is a modified testimonial for CarTalkRepair that focuses on design instead of development.'
+        },
+        {
+            background: "/images/carro-testimonial-bg.jpg",
+            backgroundOverlay: "bg-pink-300",
+            company: "Carro - Design",
+            logo: "/images/carro-logo-white.png",
+            name: "Eric Sanchez, Operations Manager",
+            quote: 'This is a different testimonial, about design work instead of development.'
+        },
+    ]
+}
 
 export default Testimonials;
--- pages/demo.tsx
+++ pages/demo.tsx
@@ -6,11 +6,26 @@ const Demo = () => {
     return (
         <main className="flex flex-col justify-center min-h-screen">
             <DemoSlider>
-                {Testimonials.map(testimonial =>
+                {Testimonials.development.map(testimonial =>
                     <DemoSlide
                         background={testimonial.background}
                         backgroundOverlay={testimonial.backgroundOverlay}
                         company={testimonial.company}
+                        key={`devTestimonialFor${testimonial.company}`}
+                        logo={testimonial.logo}
+                        name={testimonial.name}
+                        quote={testimonial.quote}
+                    />
+                )}
+            </DemoSlider>
+            <div className="mb-4"></div>
+            <DemoSlider>
+                {Testimonials.design.map(testimonial =>
+                    <DemoSlide
+                        background={testimonial.background}
+                        backgroundOverlay={testimonial.backgroundOverlay}
+                        company={testimonial.company}
+                        key={`designTestimonialFor${testimonial.company}`}
                         logo={testimonial.logo}
                         name={testimonial.name}
                         quote={testimonial.quote}
