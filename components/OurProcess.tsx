7ef5581543af8d27b61b6b2d64a6b15298cf2b86
Tyler Williams <tyler@coolsoftware.dev>
2021-04-11 16:37:35 -0600

    move classes into functions for OurProcess component

--- components/OurProcess.tsx
+++ components/OurProcess.tsx
@@ -2,10 +2,18 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"; // Import the 
 import { faSearch } from "@fortawesome/free-solid-svg-icons"; // import the icons you need
 
 const OurProcess = props => {
+    const eventDotWrapperClass = () => {
+        return `flex flex-col ${props.type % 2 === 0 ? 'md:flex-row-reverse' : 'md:flex-row'} relative self-center max-w-5xl mx-14 lg:m-auto shadow-2xl md:shadow-none`
+    }
+
+    const eventDotClass = () => {
+        return `${props.type % 2 === 0 ? 'event-dotR ml-0 md:ml-7' : 'event-dotL ml-0 md:mr-7'}  bg-alkali-500 text-center rounded-none md:rounded-full w-auto h-auto md:w-20 md:h-20 self-stretch md:self-center p-6 shadow-2xl rounded-t-md md:rounded-none`
+    }
+
     return (
         <div className="mb-16 md:mb-36">
-            <div className={`flex flex-col ${props.type % 2 === 0 ? 'md:flex-row-reverse' : 'md:flex-row'} relative self-center max-w-5xl mx-14 lg:m-auto shadow-2xl md:shadow-none`}>
-                <div className={`${props.type % 2 === 0 ? 'event-dotR ml-0 md:ml-7' : 'event-dotL ml-0 md:mr-7'}  bg-alkali-500 text-center rounded-none md:rounded-full w-auto h-auto md:w-20 md:h-20 self-stretch md:self-center p-6 shadow-2xl rounded-t-md md:rounded-none`}>
+            <div className={eventDotWrapperClass()}>
+                <div className={eventDotClass()}>
                     <FontAwesomeIcon className="text-3xl text-white" icon={faSearch}></FontAwesomeIcon>
                 </div>
                 <div className="bg-alkali-500 relative shadow-2xl text-white font-open text-xl flex align-center justify-center w-full md:w-3/12 rounded-non md:rounded-r-md">
