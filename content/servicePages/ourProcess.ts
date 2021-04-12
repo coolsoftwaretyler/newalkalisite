b86dc1b4ce93bdd7b6ef10a32d9c4f7ff98add91
Tyler Williams <tyler@coolsoftware.dev>
2021-04-11 18:07:12 -0600

    use dangerouslySetInnerHTML to start

--- content/servicePages/ourProcess.ts
+++ content/servicePages/ourProcess.ts
@@ -3,13 +3,7 @@ const OurProcess = {
         {
             title: "Project Discovery",
             icon: "faSearch",
-            step1: "Identify your objectives and your website's functionality requirements",
-            step2: "Identify your objectives and your website's functionality requirements",
-            step3: "Identify your objectives and your website's functionality requirements",
-            step4: "Identify your objectives and your website's functionality requirements",
-            step5: "Identify your objectives and your website's functionality requirements",
-            step6: "Identify your objectives and your website's functionality requirements",
-            step7: "Identify your objectives and your website's functionality requirements"
+            step1: "Identify your objectives and your website's functionality requirements<br />Identify your objectives and your website's functionality requirements<br />Identify your objectives and your website's functionality requirements<br />Identify your objectives and your website's functionality requirements<br />Identify your objectives and your website's functionality requirements<br />Identify your objectives and your website's functionality requirements<br />Identify your objectives and your website's functionality requirements"
         },
         {
             title: "Web Design",
