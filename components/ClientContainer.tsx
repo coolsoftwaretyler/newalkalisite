4ab392fc4654feb8c87c1e12aa09425db717fbb4
Tyler Williams <tyler@coolsoftware.dev>
2021-04-11 18:34:50 -0600

    try it with toLowerCase and rename component

--- components/ClientContainer.tsx
+++ components/ClientContainer.tsx
@@ -1,10 +1,10 @@
-const ClientContainers = props => {
+const ClientContainer = props => {
     return (
-        <a href={`/clients/`}>
+        <a href={`/clients/${props.name.toLowerCase()}`}>
             <li className="portfolio1 rounded-md shadow-2xl m-auto w-64 ease-in-out bg-white hover:bg-alkaligrey-700 transition duration-500 transform hover:-translate-y-1 hover:scale-110" style={{ backgroundImage: `url("/images/${props.backgroundImg}")`, backgroundRepeat: "no-repeat", backgroundSize: "cover" }}>
                 <h3 className="font-open rounded-md text-3xl text-white pl-2 pt-80 pl-5 pb-4">{props.name}</h3>
             </li>
         </a>
     )
 }
-export default ClientContainers
+export default ClientContainer
